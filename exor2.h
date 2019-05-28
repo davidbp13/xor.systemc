47cc9c8d969582406b9ae39e6999e7592740b7e4
davidbp13 <davidbp.13@gmail.com>
2019-05-28 19:36:42 +0200

    1st solution

--- exor2.h
+++ exor2.h
@@ -6,4 +6,30 @@
 // TODO: implement EXOR module here using only nand2 components
 // inputs: bool A, B
 // output bool F
+SC_MODULE(exor2)          // declare nand2 sc_module
+{
+  sc_in<bool> A, B;       // input signal ports
+  sc_out<bool> F;         // output signal ports
+  nand2 n1, n2, n3, n4;
+  sc_signal<bool> s1, s2, s3;
 
+  SC_CTOR(exor2)          // constructor for exor2
+  {
+    n1.A.bind(A);
+    n1.B.bind(B);
+    n1.F.bind(s1);
+
+    n2.A.bind(A);
+    n2.B.bind(s1);
+    n2.F.bind(s2);
+
+    n3.A.bind(s1);
+    n3.B.bind(B);
+    n3.F.bind(s3);
+
+    n4.A.bind(s2);
+    n4.B.bind(s3);
+    n4.F.bind(F);
+    //sensitive << A << B;  // sensitivity list
+  }
+};
